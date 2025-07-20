312b97a1356f71d4c905ddf70cafd577e6b1b0db
Zubair Khan <khan576335@gmail.com>
2025-07-20 16:14:47 +0530

    add cors for vercel deployment

--- PersonalFinanceTracker/src/main/java/com/demo/Expense/Config/WebConfig.java
+++ PersonalFinanceTracker/src/main/java/com/demo/Expense/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // Allow CORS for any API under "/api"
-                .allowedOrigins("http://localhost:3000")  // Allow frontend to access the backend
+                .allowedOrigins("http://localhost:3000", "https://money-tris.vercel.app")  // Allow frontend to access the backend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow necessary HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials (cookies, sessions, etc.)
